03ac03ec9b7bf405ce754bef3e37666c103f9830
Max Zimmermann <Max Zimmermann@DESKTOP-5OOBUIK>
2024-02-04 15:00:07 +0100

    added page

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Home(models.Model):
+    image = models.ImageField(upload_to='home/')
+    name = models.CharField(max_length=30)
+    job_role = models.CharField(max_length=30)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Home
 
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "portfolio/home.html")
+        home = Home.objects.first()
+
+        context = {"home": home}
+
+        return render(request, "portfolio/home.html", context=context)
