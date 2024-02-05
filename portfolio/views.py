f30ab814b9ac77360e16432190cd70c1a91950bf
Max Zimmermann <Max Zimmermann@DESKTOP-5OOBUIK>
2024-02-05 22:41:58 +0100

    added page

--- portfolio/views.py
+++ portfolio/views.py
@@ -10,7 +10,11 @@ class HomeView(ListView):
     template_name = 'portfolio/home.html'
     model = Home
 
+    def get_queryset(self):
+        # Fetch all references with related images, features, and tags
+        queryset = super().get_queryset().prefetch_related("links")
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["header"] = Header.objects.prefetch_related('links').all()
         return context
